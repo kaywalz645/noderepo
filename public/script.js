908b9fdcd4d115df0267b28de1e9b0f14e728589
kaywalz645 <kaybalz645@gmail.com>
2023-11-06 02:37:50 -0500

    update

--- public/script.js
+++ public/script.js
@@ -1,1 +1,34 @@
-console.log("is this working?");
+const displayPeople = async () => {
+  let response = await fetch("http://localhost:3000/api/people");
+  let peopleJSON = await response.json();
+  let peopleDiv = document.getElementById("people");
+
+  peopleJSON.forEach((people) => {
+    let section = document.createElement("section");
+    peopleDiv.append(section);
+
+    let h3 = document.createElement("h3");
+    section.append(h3);
+    h3.innerHTML = people.name;
+
+    let p = document.createElement("p");
+    p.innerHTML =
+      "Age: " +
+      people.age +
+      "<br>Favorite Color: " +
+      people.fcolor +
+      "<br>Activity: " +
+      people.activity +
+      "<br>Favorite Foods: " +
+      people.fFoods;
+    section.append(p);
+
+    let img = document.createElement("img");
+    section.append(img);
+    img.src = "http://localhost:3000/" + people.img;
+  });
+};
+
+window.onload = () => {
+  displayPeople();
+};
--- server.js
+++ server.js
@@ -1,19 +1,63 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 });
 
-app.get("/api/cakes", (req, res) => {
-  const cakes = [
-    "birthday cake",
-    "red velvet cake",
-    "chocolate cakes",
-    "cheese cake",
-  ];
-  res.send(cakes);
-});
 app.listen(3000, () => {
-  console.log("im listening");
+    console.log("im listening");
+  });
+
+
+app.get("/api/people", (req, res) => {
+  const people = [];
+  people[0] = {
+    name: "Samatha Young",
+    age: "47",
+    fcolor: "Green",
+    activity: "Knitting",
+    fFoods: [" Yogurt", " Candy", " Pizza"],
+    img: "images/old-lady.jpeg",
+  };
+
+  people[1] = {
+    name: "Samuel Old",
+    age: "2",
+    fcolor: "Orange",
+    activity: "Throwing things",
+    fFoods: [" Peas", " Yogurt", " Banana"],
+    img: "images/baby-boy.jpg",
+  };
+
+  people[2] = {
+    name: "Henry Foul",
+    age: "20",
+    fcolor: "Yellow",
+    activity: "Running",
+    fFoods: [" Steak", " Mashed Potatos", " Beef Jerky"],
+    img: "images/man.jpg",
+  };
+
+  people[3] = {
+    name: "Katelyn Grow",
+    age: "16",
+    fcolor: "Purple",
+    activity: "Dancing",
+    fFoods: [" Cereal", " Spagetti", " Bread"],
+    img: "images/teen-girl.jpg",
+  };
+
+  people[4] = {
+    name: "Aaron Steve",
+    age: "32",
+    fcolor: "Brown",
+    activity: "Coding",
+    fFoods: [" Ramen", " Coffee", " Pasta"],
+    img: "images/aged-guy.jpg",
+  };
+
+  res.json(people);
 });
+
